650da9ab8cc11bedd5d67ec4afcac7144e21c811
David Goguen <dgogu058@uottawa.ca>
2018-01-30 11:10:39 -0500

    Work on UI

--- Custom Mouse/FrmMain.Designer.cs
+++ Custom Mouse/FrmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Custom_Mouse
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.nfyTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
+            // 
+            // nfyTrayIcon
+            // 
+            this.nfyTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("nfyTrayIcon.Icon")));
+            this.nfyTrayIcon.Text = "notifyIcon1";
+            this.nfyTrayIcon.Visible = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 472);
+            this.ClientSize = new System.Drawing.Size(1194, 708);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon nfyTrayIcon;
     }
 }
 
--- Custom Mouse/Program.cs
+++ Custom Mouse/Program.cs
@@ -16,7 +16,7 @@ namespace Custom_Mouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
