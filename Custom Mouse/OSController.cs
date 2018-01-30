08872bbfedde9ee3fa07858fa4f684e09123f0ea
David Goguen <dgogu058@uottawa.ca>
2018-01-30 10:16:41 -0500

    Created needed classes

---
+++ Custom Mouse/OSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Custom_Mouse
+{
+    class OSController
+    {
+        public void OpenOnScreenKeyboard()
+        {
+            Process.Start("osk.exe");
+        }
+    }
+}
