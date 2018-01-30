da8d7b478751ae1460fcb4fd3d8e15dc9989fcd9
David Goguen <dgogu058@uottawa.ca>
2018-01-30 14:44:31 -0500

    Work on Windows service

---
+++ CustomMouseService/ICustomMouseServiceCommunicator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CustomMouseService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICustomMouseServiceCommunicator" in both code and config file together.
+    [ServiceContract]
+    public interface ICustomMouseServiceCommunicator
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
