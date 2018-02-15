1bcb6bdbca6db006240556a6037c4fcb03ce2ea8
David Goguen <dgogu058@uottawa.ca>
2018-02-15 12:24:31 -0500

    Work on UI

--- CustomMouseGUI/FrmMain.Designer.cs
+++ CustomMouseGUI/FrmMain.Designer.cs
@@ -31,12 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.nfyTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
-            this.pnlInputSelection = new System.Windows.Forms.Panel();
             this.cxtTrayIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.mnuOpenControlCenter = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlInputSelection = new System.Windows.Forms.Panel();
+            this.btnJoystick = new System.Windows.Forms.Button();
+            this.btnButton1 = new System.Windows.Forms.Button();
+            this.btnButton2 = new System.Windows.Forms.Button();
+            this.btnButton3 = new System.Windows.Forms.Button();
+            this.btnButton4 = new System.Windows.Forms.Button();
+            this.btnButton5 = new System.Windows.Forms.Button();
+            this.btnButton6 = new System.Windows.Forms.Button();
             this.cxtTrayIcon.SuspendLayout();
+            this.pnlInputSelection.SuspendLayout();
             this.SuspendLayout();
             // 
             // nfyTrayIcon
@@ -46,16 +54,6 @@
             this.nfyTrayIcon.Text = "Custom Mouse Control Center";
             this.nfyTrayIcon.Visible = true;
             this.nfyTrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.nfyTrayIcon_MouseDoubleClick);
-            // 
-            // pnlInputSelection
-            // 
-            this.pnlInputSelection.AutoScroll = true;
-            this.pnlInputSelection.BackColor = System.Drawing.Color.Transparent;
-            this.pnlInputSelection.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlInputSelection.Location = new System.Drawing.Point(0, 0);
-            this.pnlInputSelection.Name = "pnlInputSelection";
-            this.pnlInputSelection.Size = new System.Drawing.Size(391, 693);
-            this.pnlInputSelection.TabIndex = 0;
             // 
             // cxtTrayIcon
             // 
@@ -74,6 +72,11 @@
             this.mnuOpenControlCenter.Text = "Open Control Center";
             this.mnuOpenControlCenter.Click += new System.EventHandler(this.mnuOpenControlCenter_Click);
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(275, 6);
+            // 
             // mnuExit
             // 
             this.mnuExit.Name = "mnuExit";
@@ -81,16 +84,132 @@
             this.mnuExit.Text = "Exit";
             this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
             // 
-            // toolStripSeparator1
+            // pnlInputSelection
             // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(275, 6);
+            this.pnlInputSelection.BackColor = System.Drawing.Color.Transparent;
+            this.pnlInputSelection.Controls.Add(this.btnButton6);
+            this.pnlInputSelection.Controls.Add(this.btnButton5);
+            this.pnlInputSelection.Controls.Add(this.btnButton4);
+            this.pnlInputSelection.Controls.Add(this.btnButton3);
+            this.pnlInputSelection.Controls.Add(this.btnButton2);
+            this.pnlInputSelection.Controls.Add(this.btnButton1);
+            this.pnlInputSelection.Controls.Add(this.btnJoystick);
+            this.pnlInputSelection.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlInputSelection.Location = new System.Drawing.Point(0, 0);
+            this.pnlInputSelection.Name = "pnlInputSelection";
+            this.pnlInputSelection.Size = new System.Drawing.Size(391, 693);
+            this.pnlInputSelection.TabIndex = 1;
+            // 
+            // btnJoystick
+            // 
+            this.btnJoystick.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnJoystick.FlatAppearance.BorderSize = 0;
+            this.btnJoystick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJoystick.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoystick.ForeColor = System.Drawing.Color.White;
+            this.btnJoystick.Location = new System.Drawing.Point(0, 0);
+            this.btnJoystick.Name = "btnJoystick";
+            this.btnJoystick.Size = new System.Drawing.Size(391, 99);
+            this.btnJoystick.TabIndex = 0;
+            this.btnJoystick.TabStop = false;
+            this.btnJoystick.Text = "Joystick";
+            this.btnJoystick.UseVisualStyleBackColor = false;
+            // 
+            // btnButton1
+            // 
+            this.btnButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton1.FlatAppearance.BorderSize = 0;
+            this.btnButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton1.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton1.ForeColor = System.Drawing.Color.White;
+            this.btnButton1.Location = new System.Drawing.Point(0, 99);
+            this.btnButton1.Name = "btnButton1";
+            this.btnButton1.Size = new System.Drawing.Size(391, 99);
+            this.btnButton1.TabIndex = 0;
+            this.btnButton1.TabStop = false;
+            this.btnButton1.Text = "Button 1";
+            this.btnButton1.UseVisualStyleBackColor = false;
+            // 
+            // btnButton2
+            // 
+            this.btnButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton2.FlatAppearance.BorderSize = 0;
+            this.btnButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton2.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton2.ForeColor = System.Drawing.Color.White;
+            this.btnButton2.Location = new System.Drawing.Point(0, 198);
+            this.btnButton2.Name = "btnButton2";
+            this.btnButton2.Size = new System.Drawing.Size(391, 99);
+            this.btnButton2.TabIndex = 0;
+            this.btnButton2.TabStop = false;
+            this.btnButton2.Text = "Button 2";
+            this.btnButton2.UseVisualStyleBackColor = false;
+            // 
+            // btnButton3
+            // 
+            this.btnButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton3.FlatAppearance.BorderSize = 0;
+            this.btnButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton3.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton3.ForeColor = System.Drawing.Color.White;
+            this.btnButton3.Location = new System.Drawing.Point(0, 297);
+            this.btnButton3.Name = "btnButton3";
+            this.btnButton3.Size = new System.Drawing.Size(391, 99);
+            this.btnButton3.TabIndex = 0;
+            this.btnButton3.TabStop = false;
+            this.btnButton3.Text = "Button 3";
+            this.btnButton3.UseVisualStyleBackColor = false;
+            // 
+            // btnButton4
+            // 
+            this.btnButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton4.FlatAppearance.BorderSize = 0;
+            this.btnButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton4.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton4.ForeColor = System.Drawing.Color.White;
+            this.btnButton4.Location = new System.Drawing.Point(0, 396);
+            this.btnButton4.Name = "btnButton4";
+            this.btnButton4.Size = new System.Drawing.Size(391, 99);
+            this.btnButton4.TabIndex = 0;
+            this.btnButton4.TabStop = false;
+            this.btnButton4.Text = "Button 4";
+            this.btnButton4.UseVisualStyleBackColor = false;
+            // 
+            // btnButton5
+            // 
+            this.btnButton5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton5.FlatAppearance.BorderSize = 0;
+            this.btnButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton5.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton5.ForeColor = System.Drawing.Color.White;
+            this.btnButton5.Location = new System.Drawing.Point(0, 495);
+            this.btnButton5.Name = "btnButton5";
+            this.btnButton5.Size = new System.Drawing.Size(391, 99);
+            this.btnButton5.TabIndex = 0;
+            this.btnButton5.TabStop = false;
+            this.btnButton5.Text = "Button 5";
+            this.btnButton5.UseVisualStyleBackColor = false;
+            // 
+            // btnButton6
+            // 
+            this.btnButton6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnButton6.FlatAppearance.BorderSize = 0;
+            this.btnButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnButton6.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnButton6.ForeColor = System.Drawing.Color.White;
+            this.btnButton6.Location = new System.Drawing.Point(0, 594);
+            this.btnButton6.Name = "btnButton6";
+            this.btnButton6.Size = new System.Drawing.Size(391, 99);
+            this.btnButton6.TabIndex = 0;
+            this.btnButton6.TabStop = false;
+            this.btnButton6.Text = "Button 6";
+            this.btnButton6.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
             this.ClientSize = new System.Drawing.Size(1213, 693);
             this.Controls.Add(this.pnlInputSelection);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -102,6 +221,7 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.cxtTrayIcon.ResumeLayout(false);
+            this.pnlInputSelection.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -109,11 +229,18 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon nfyTrayIcon;
-        private System.Windows.Forms.Panel pnlInputSelection;
         private System.Windows.Forms.ContextMenuStrip cxtTrayIcon;
         private System.Windows.Forms.ToolStripMenuItem mnuOpenControlCenter;
         private System.Windows.Forms.ToolStripMenuItem mnuExit;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Panel pnlInputSelection;
+        private System.Windows.Forms.Button btnButton6;
+        private System.Windows.Forms.Button btnButton5;
+        private System.Windows.Forms.Button btnButton4;
+        private System.Windows.Forms.Button btnButton3;
+        private System.Windows.Forms.Button btnButton2;
+        private System.Windows.Forms.Button btnButton1;
+        private System.Windows.Forms.Button btnJoystick;
     }
 }
 
--- CustomMouseGUI/FrmMain.cs
+++ CustomMouseGUI/FrmMain.cs
@@ -20,59 +20,6 @@ namespace CustomMouseGUI
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            string[] buttonNames = new string[12];
-            buttonNames[0] = "Joystick";
-
-            for (int i = 1; i < 12; i++)
-            {
-                buttonNames[i] = "Button " + i;
-            }
-
-            Button lastButton = null;
-            for (int i = 0; i < 12; i++)
-            {
-                Button toAdd = new Button();
-
-                if (i != 0)
-                {
-                    toAdd.Location = new Point(0, lastButton.Location.Y + lastButton.Height);
-                }
-
-                toAdd.Text = buttonNames[i];
-
-                toAdd.Font = new Font(FontFamily.GenericSansSerif, 20);
-                toAdd.ForeColor = Color.White;
-                toAdd.Size = new Size(pnlInputSelection.Width, 60);
-                toAdd.BackColor = Color.DarkRed;
-                toAdd.FlatStyle = FlatStyle.Flat;
-                toAdd.FlatAppearance.BorderSize = 0;
-                toAdd.FlatAppearance.BorderColor = Color.DarkRed;
-
-                toAdd.TabStop = false;
-
-                toAdd.Click += new EventHandler(ConfigureButton_Click);
-                pnlInputSelection.Controls.Add(toAdd);
-                lastButton = toAdd;
-            }
-
-            if (pnlInputSelection.VerticalScroll.Enabled)
-            {
-                pnlInputSelection.Width += SystemInformation.VerticalScrollBarWidth;
-            }
-        }
-
-        private void ConfigureButton_Click(object sender, EventArgs e)
-        {
-            Button clicked = (Button)sender;
-
-            foreach (Button button in pnlInputSelection.Controls)
-            {
-                if (button != clicked)
-                {
-                    button.BackColor = Color.DarkRed;
-                }
-            }
-            clicked.BackColor = Color.Red;
         }
 
         private void mnuExit_Click(object sender, EventArgs e)
