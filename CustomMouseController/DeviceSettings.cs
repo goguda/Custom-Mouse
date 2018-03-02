02e51157b04453df346a0fa5773703b7af9064e4
David Goguen <dgogu058@uottawa.ca>
2018-03-01 19:28:26 -0500

    Added class to hold settings

---
+++ CustomMouseController/DeviceSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMouseController
+{
+    [Serializable]
+    public class DeviceSettings
+    {
+        private static volatile DeviceSettings instance;
+
+        private ButtonSetting[] buttonSettings;
+
+        private DeviceSettings()
+        {
+        }
+
+        public static DeviceSettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DeviceSettings();
+                }
+                return instance;
+            }
+        }
+    }
+}
