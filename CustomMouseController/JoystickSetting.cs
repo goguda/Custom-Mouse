823658a37aef6bf14f7adfe5d78b7624f92ef954
David Goguen <dgogu058@uottawa.ca>
2018-03-13 15:25:42 -0400

    Creation of JoystickSetting class

---
+++ CustomMouseController/JoystickSetting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMouseController
+{
+    public class JoystickSetting
+    {
+        private int speedMultiplier;
+        private int sensitivityMultiplier;
+
+        public JoystickSetting()
+        {
+            speedMultiplier = 1;
+            sensitivityMultiplier = 1;
+        }
+
+        public int SpeedMultiplier
+        {
+            get
+            {
+                return speedMultiplier;
+            }
+            set
+            {
+                speedMultiplier = value;
+            }
+        }
+
+        public int SensitivityMultiplier
+        {
+            get
+            {
+                return sensitivityMultiplier;
+            }
+            set
+            {
+                speedMultiplier = value;
+            }
+        }
+    }
+}
